219b8f3d6e2cd7359aff236aae94024d678949fa
AmmarIrfanKotwal <123722868+AmmarKotwal@users.noreply.github.com>
2025-06-20 02:32:13 +0500

    Functions Assignment

--- BMI.R
+++ BMI.R
@@ -48,26 +48,61 @@
 # area_foramulas(length2,breadth2)
 
 
-BMI_foramulas <- function(){
-    height <- as.numeric(readline("Enter No Of Height In Meters: "))
-    weight <- as.numeric(readline("Enter No Of Weight In Kg: "))
-    BMI <- weight / (height^2)
-    
-    if(BMI < 18.5){
-      print("UnderWeight")
-    }else if(BMI > 18.5 && BMI < 24.9){
-      print("Normal")
-    }else if(BMI > 25 && BMI < 29.9){
-      print("OverWeight")
-    }else if(BMI > 30){
-      print("Obese")
-    }else{
-      print("Invalid Data")
-    }
+# BMI_foramulas <- function(){
+#     height <- as.numeric(readline("Enter No Of Height In Meters: "))
+#     weight <- as.numeric(readline("Enter No Of Weight In Kg: "))
+#     BMI <- weight / (height^2)
+#     
+#     if(BMI < 18.5){
+#       print("UnderWeight")
+#     }else if(BMI > 18.5 && BMI < 24.9){
+#       print("Normal")
+#     }else if(BMI > 25 && BMI < 29.9){
+#       print("OverWeight")
+#     }else if(BMI > 30){
+#       print("Obese")
+#     }else{
+#       print("Invalid Data")
+#     }
+# 
+#     cat("Your BMI Is: " , BMI)
+#   }
+# 
+#   BMI_foramulas()
 
-    cat("Your BMI Is: " , BMI)
-  }
 
-  BMI_foramulas()
+# Temperature Converter:-
+
+# temperatureConverter <- function() {
+#   celsius <- as.numeric(readline("Enter Temperature in Celsius: "))
+#   
+#   fahrenheit <- (celsius * 9/5) + 32
+#   kelvin <- celsius + 273.15
+#   
+#   cat("Temperature in Fahrenheit: ", fahrenheit, "\nTemperature in Kelvin: ", kelvin)
+# }
+# 
+# temperatureConverter()
+
+
+# Simple Interest Calculator:-
+
+# simpleInterestCalculator <- function() {
+#   p <- as.numeric(readline("Enter Principal Amount: "))
+#   r <- as.numeric(readline("Enter Rate of Interest: "))
+#   t <- as.numeric(readline("Enter Time in Years: "))
+#   
+#   si <- (p * r * t) / 100
+#   
+#   cat("Simple Interest is: ", si)
+# }
+# 
+# simpleInterestCalculator()
+
+
+
+
+
+
   
   
